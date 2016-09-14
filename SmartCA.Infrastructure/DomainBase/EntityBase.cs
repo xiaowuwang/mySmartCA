219edd9c5413a5e0cbfb59613d6557b9d544f334
xiaowuwang <stevenwang@hotmail.co.nz>
2016-09-14 12:12:36 +1200

    Add IEntity.cs ....

--- SmartCA.Infrastructure/DomainBase/EntityBase.cs
+++ SmartCA.Infrastructure/DomainBase/EntityBase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace mySmartCA.Infrastructure.DomainBase
+namespace SmartCA.Infrastructure.DomainBase
 {
     public abstract class EntityBase
     {
---
+++ SmartCA.Model/Projects/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using mySmartCA.Infrastructure.RepositoryFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCA.Model.Projects
+{
+    public interface IProjectRepository : IRepository<Project>
+    {
+        IList<Project> FindBy(object sector, object segment, bool completed);
+    }
+}
